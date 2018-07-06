0e1b1935095a544ceff4aee4a817a177076d3279
Nik G <subzeromedia@gmail.com>
2018-07-06 09:39:45 -0500

    Add avatar options

--- src/App.js
+++ src/App.js
@@ -10,6 +10,20 @@ class App extends Component {
   constructor() {
     super()
     this.state = {};
+    this.options = {
+      avatarStyle: ['Circle', 'Transparent'],
+      topType: ['Eyepatch', 'Hat', 'Hijab', 'LongHairBigHair', 'LongHairBob', 'LongHairBun', 'LongHairCurly', 'LongHairCurvy', 'LongHairDreads', 'LongHairFrida', 'LongHairFro', 'LongHairFroBand', 'LongHairMiaWallace', 'LongHairNotTooLong', 'LongHairShavedSides', 'LongHairStraight', 'LongHairStraight2', 'LongHairStraightStrand', 'NoHair', 'ShortHairDreads01', 'ShortHairDreads02', 'ShortHairFrizzle', 'ShortHairShaggy', 'ShortHairShaggyMullet', 'ShortHairShortCurly', 'ShortHairShortFlat', 'ShortHairShortRound', 'ShortHairShortWaved', 'ShortHairSides', 'ShortHairTheCaesar', 'ShortHairTheCaesarSidePart', 'Turban', 'WinterHat1', 'WinterHat2', 'WinterHat3', 'WinterHat4'],
+      hatColor: ['Black', 'Blue01', 'Blue02', 'Blue03', 'Gray01', 'Gray02', 'Heather', 'PastelBlue', 'PastelGreen', 'PastelOrange', 'PastelRed', 'PastelYellow', 'Pink', 'Red', 'White'],
+      accessoriesType: ['Blank', 'Kurt', 'Prescription01', 'Prescription02', 'Round', 'Sunglasses', 'Wayfarers'],
+      hairColor: ['Auburn', 'Black', 'Blonde', 'BlondeGolden', 'Brown', 'BrownDark', 'PastelPink', 'Platinum', 'Red', 'SilverGray'],
+      facialHairType: ['BeardLight', 'BeardMagestic', 'BeardMedium', 'Blank', 'MoustacheFancy', 'MoustacheMagnum'],
+      clotheType: ['BlazerShirt', 'BlazerSweater', 'CollarSweater', 'GraphicShirt', 'Graphics', 'Hoodie', 'Overall', 'ShirtCrewNeck', 'ShirtScoopNeck', 'ShirtVNeck'],
+      clotheColor: ['Black', 'Blue01', 'Blue02', 'Blue03', 'Gray01', 'Gray02', 'Heather', 'PastelBlue', 'PastelGreen', 'PastelOrange', 'PastelRed', 'PastelYellow', 'Pink', 'Red', 'White'],
+      eyeType: ['Close', 'Cry', 'Default', 'Dizzy', 'EyeRoll', 'Happy', 'Hearts', 'Side', 'Squint', 'Surprised', 'Wink', 'WinkWacky'],
+      eyebrowType: ['Angry', 'AngryNatural', 'Default', 'DefaultNatural', 'FlatNatural', 'FrownNatural', 'RaisedExcited', 'RaisedExcitedNatural', 'SadConcerned', 'SadConcernedNatural', 'UnibrowNatural', 'UpDown', 'UpDownNatural'],
+      mouthType: ['Concerned', 'Default', 'Disbelief','Eating', 'Grimace', 'Sad', 'ScreamOpen', 'Serious', 'Smile', 'Tongue', 'Twinkle', 'Vomit'],
+      skinColor: ['Tanned', 'Yellow', 'Pale', 'Light', 'Brown', 'DarkBrown', 'Black']
+    }
   }
 
   componentDidMount() {
@@ -21,6 +35,7 @@ class App extends Component {
   }
 
   render() {
+    const {avatarStyle, topType, hatColor, accessoriesType, hairColor, facialHairType, clotheType, clotheColor, eyeType, eyebrowType, mouthType, skinColor} = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -31,17 +46,18 @@ class App extends Component {
         <p className="App-intro">
           <Avatar
             style={{width: '300px', height: '300px'}}
-            avatarStyle='Circle'
-            topType='LongHairMiaWallace'
-            accessoriesType='Prescription02'
-            hairColor='BrownDark'
-            facialHairType='Blank'
-            clotheType='Hoodie'
-            clotheColor='PastelBlue'
-            eyeType='Happy'
-            eyebrowType='Default'
-            mouthType='Smile'
-            skinColor='Light'
+            avatarStyle={avatarStyle}
+            topType={topType}
+            hatColor={hatColor}
+            accessoriesType={accessoriesType}
+            hairColor={hairColor}
+            facialHairType={facialHairType}
+            clotheType={clotheType}
+            clotheColor={clotheColor}
+            eyeType={eyeType}
+            eyebrowType={eyebrowType}
+            mouthType={mouthType}
+            skinColor={skinColor}
           />
         </p>
       </div>
